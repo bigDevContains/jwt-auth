1f48ced945c5814e01a687ed049aece52a705502
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2023-08-30 23:09:33 +0100

    user struct created

--- models/userModel.go
+++ models/userModel.go
@@ -1,3 +1,20 @@
 package models
 
-import()
+import(
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID		primitive.ObjectID		`bson:"_id"`
+	First_name	*string				`json:"first_name" validate:"required, min=2, max=100"`
+	Last_name	*string				`json:"last_name" validate:"required, min=2, max=100`
+	Password
+	Email
+	Phone
+	Token
+	User_type
+	Refresh_token
+	Created_at
+	Updated_at
+	User_id
+}
