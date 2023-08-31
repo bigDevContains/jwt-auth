e353a4d6b0c03f0a730f28268244873aaabfbb65
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2023-09-01 00:07:56 +0100

    middleware created

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,12 @@
 package routes
+
+import(
+	controller "github.com/bigDevContains/golang-jwt-project/controllers"
+	
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incoming Routes *gin.Engine){
+	incomingToutes.POST("users/signup", controller.Signup())
+	incomingROutes.POST("users/login", controller.Login())
+}
