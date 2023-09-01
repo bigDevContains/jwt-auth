5bfd9d5742e8edcc23e6aae8e2bfa7d4665e3b42
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2023-09-01 01:15:14 +0100

    userController logic

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,11 +1,11 @@
 package routes
 
 import(
-	controller "github.com/bigDevContains/golang-jwt-project/controllers"
+	controllers "github.com/bigDevContains/golang-jwt-project/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func AuthRoutes(incomingRoutes *gin.Engine){
-	incomingRoutes.POST("users/signup", controller.Signup())
-	incomingRoutes.POST("users/login", controller.Login())
+	incomingRoutes.POST("users/signup", controllers.Signup())
+	incomingRoutes.POST("users/login", controllers.Login())
 }
